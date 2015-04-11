8873346997a214c06a6251323ec473b2ce98a3db
bl4ckscor3 <donalddani9612@gmx.de>
2015-04-11 22:33:58 +0200

    only open/close the entrance when no players are in the region

--- src/bl4ckscor3/plugin/secutilities/features/DonatorRoomEntrance.java
+++ src/bl4ckscor3/plugin/secutilities/features/DonatorRoomEntrance.java
@@ -11,17 +11,28 @@ import com.mewin.WGRegionEvents.events.RegionLeaveEvent;
 
 public class DonatorRoomEntrance implements Listener
 {
+	private static int playersInRegion = 0;
+	
 	@EventHandler
 	public void onRegionEntered(RegionEnteredEvent event) throws IOException
 	{
-		if(event.getRegion().getId().equalsIgnoreCase("donatorroomentrance"))
-			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "setblock -246 55 288 minecraft:air");
+			if(event.getRegion().getId().equalsIgnoreCase("donatorroomentrance"))
+			{
+				if(playersInRegion == 0)
+					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "setblock -246 55 288 minecraft:air");
+				playersInRegion++;
+			}
 	}
 	
 	@EventHandler
 	public void onRegionLeave(RegionLeaveEvent event)
 	{
 		if(event.getRegion().getId().equalsIgnoreCase("donatorroomentrance"))
-			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "setblock -246 55 288 minecraft:redstone_block");
+		{
+			playersInRegion--;
+			
+			if(playersInRegion == 0)
+				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "setblock -246 55 288 minecraft:redstone_block");
+		}
 	}
 }
