79a3a4ac994f2080508919d84107f0b12a97514a
bl4ckscor3 <donalddani9612@gmx.de>
2015-06-24 10:37:33 +0200

    town restrictions

--- src/bl4ckscor3/plugin/secutilities/features/listener/BlockPlaceListener.java
+++ src/bl4ckscor3/plugin/secutilities/features/listener/BlockPlaceListener.java
@@ -11,11 +11,31 @@ import org.bukkit.event.block.BlockPlaceEvent;
 public class BlockPlaceListener implements Listener
 {
 	public static List<String> blockPlaceOff = new ArrayList<String>();
-	
+
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event) throws IOException
 	{
 		if(blockPlaceOff.contains(event.getPlayer().getName()))
 			event.setCancelled(true);
+		
+		if(RegionEnteredLeftListener.isPlayerInRegion(event.getPlayer().getName(), "town"))
+		{
+			int newId = 0;
+			
+			switch(event.getBlock().getTypeId())
+			{
+				case 197: //reinforced iron bars
+					newId = 101;
+					break;
+				case 200: //reinforced glass pane
+					newId = 102;
+					break;
+				case 1: //reinforced stone
+					newId = 98;
+					break;
+			}
+			
+			event.getBlock().setTypeId(newId);
+		}
 	}
 }
