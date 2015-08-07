2485cb41ed39ddf692972965fb43be75319b3531
bl4ckscor3 <donalddani9612@gmx.de>
2015-08-07 13:54:11 +0200

    another fix

--- src/bl4ckscor3/plugin/secutilities/listener/PlayerInteractListener.java
+++ src/bl4ckscor3/plugin/secutilities/listener/PlayerInteractListener.java
@@ -21,12 +21,12 @@ import bl4ckscor3.plugin.bl4ckkitCore.core.bl4ckkitCore;
 public class PlayerInteractListener implements Listener
 {
 	private static Plugin plugin;
-	
+
 	public PlayerInteractListener(Plugin pl)
 	{
 		plugin = pl;
 	}
-	
+
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event)
 	{
@@ -37,29 +37,31 @@ public class PlayerInteractListener implements Listener
 			if(hand.getType() == Material.PAPER && hand.getItemMeta().getLore().get(0).equals("Location Tool"))
 			{
 				Location l = event.getClickedBlock().getLocation();
-				
+
 				bl4ckkitCore.getMessageManager().sendChatMessage(event.getPlayer(), plugin, "x: " + l.getBlockX() + " y: " + l.getBlockY() + " z: " + l.getBlockZ());
 				event.setCancelled(true);
 			}
 		}
-		
+
 		if(event.getAction() == Action.RIGHT_CLICK_BLOCK && isSign(event.getClickedBlock()))
 		{
 			if(event.getPlayer().hasPermission("secutil.cwarp.warp"))
 			{
 				String[] lines = ((Sign)event.getClickedBlock().getState()).getLines();
-				
-				if(lines[0].equalsIgnoreCase("" + ChatColor.DARK_RED + "[CWarp]"))
+
+				if(lines[0].equalsIgnoreCase(ChatColor.DARK_RED + "[CWarp]"))
 				{
 					bl4ckkitCore.getMessageManager().sendChatMessage(event.getPlayer(), plugin, "This warp does not exist.");
 					return;
 				}
-				
-				File f = new File(plugin.getDataFolder(), "/warps/" + lines[1] + ".yml");
-				YamlConfiguration yaml = YamlConfiguration.loadConfiguration(f);
+				else if(lines[0].equalsIgnoreCase(ChatColor.DARK_GREEN + "[CWarp]"))
+				{
+					File f = new File(plugin.getDataFolder(), "/warps/" + lines[1] + ".yml");
+					YamlConfiguration yaml = YamlConfiguration.loadConfiguration(f);
 
-				event.getPlayer().teleport(new Location(Bukkit.getWorld("creative"), yaml.getDouble("x"), yaml.getDouble("y"), yaml.getDouble("z"), (float)yaml.getDouble("yaw"), (float)yaml.getDouble("pitch")));
-				bl4ckkitCore.getMessageManager().sendChatMessage(event.getPlayer(), plugin, "Warped to " + ChatColor.AQUA + lines[1] + ChatColor.WHITE + ".");
+					event.getPlayer().teleport(new Location(Bukkit.getWorld("creative"), yaml.getDouble("x"), yaml.getDouble("y"), yaml.getDouble("z"), (float)yaml.getDouble("yaw"), (float)yaml.getDouble("pitch")));
+					bl4ckkitCore.getMessageManager().sendChatMessage(event.getPlayer(), plugin, "Warped to " + ChatColor.AQUA + lines[1] + ChatColor.WHITE + ".");
+				}
 			}
 		}
 	}
